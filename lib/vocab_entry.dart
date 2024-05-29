ef208511461c4a33ee905d2892dedcf5d2b22766
Ola Gawell <ola.gawell@devoteam.com>
2024-05-29 16:05:38 +0200

    Added persistance

--- lib/vocab_entry.dart
+++ lib/vocab_entry.dart
@@ -5,9 +5,21 @@ class VocabEntry {
   final String english;
   final bool hasDash;
 
-  VocabEntry({
-    required this.swedish,
-    required this.english,
-    required this.hasDash,
-  });
+  VocabEntry({required this.swedish, required this.english, required this.hasDash});
+
+  factory VocabEntry.fromJson(Map<String, dynamic> json) {
+    return VocabEntry(
+      swedish: json['swedish'],
+      english: json['english'],
+      hasDash: json['hasDash'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'swedish': swedish,
+      'english': english,
+      'hasDash': hasDash,
+    };
+  }
 }
