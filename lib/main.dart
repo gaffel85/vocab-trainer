db8ccc944f625638a39db35db508a892c28789bf
Ola Gawell <ola.gawell@devoteam.com>
2024-06-09 08:08:59 +0200

    Results on main screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'vocab_provider.dart';
 import 'quiz_screen.dart';
+import 'vocab_entry.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,11 +28,13 @@ class MyApp extends StatelessWidget {
 
 class VocabHomePage extends StatelessWidget {
   // Check if the app is running in development mode
-  static const bool isDevelopment = bool.fromEnvironment('dart.vm.product') == false;
+  static const bool isDevelopment =
+      bool.fromEnvironment('dart.vm.product') == false;
 
   // Provide example text only in development mode
   final TextEditingController _controller = TextEditingController(
-      text: isDevelopment ? '''1. trust                -    lita på
+      text: isDevelopment
+          ? '''1. trust                -    lita på
 2. drawing            -    teckning
 3. ruler                -    linjal
 4. parallel            -    parallell
@@ -49,8 +52,44 @@ class VocabHomePage extends StatelessWidget {
 16. rubber            -    gummi
 17. touch together        -    föra samman
 18. cooperate            -    samarbeta
-''' : ''
-  );
+'''
+          : '');
+
+  Color _getEntryColor(Result result) {
+    int practiceScore = result.needsMorePracticeScore();
+    if (practiceScore == 0) {
+      return Colors.green;
+    } else if (practiceScore == 1) {
+      return Colors.yellow;
+    } else if (practiceScore == 7) {
+      return Colors.red;
+    } else {
+      // Calculate gradient color between yellow and red
+      final int red = 255;
+      final int green = 255 -
+          ((practiceScore - 1) *
+              31); // 31 is the step size between yellow and red
+      return Color.fromARGB(255, red, green, 0);
+    }
+  }
+
+  Icon _getPracticeIcon(int practiceScore) {
+    if (practiceScore == 0) {
+      return Icon(Icons.check_circle, color: Colors.green);
+    } else if (practiceScore == 1) {
+      return Icon(Icons.check_circle, color: Colors.yellow);
+    } else if (practiceScore == 7) {
+      return Icon(Icons.error, color: Colors.red);
+    } else {
+      return Icon(Icons.warning,
+          color: _getEntryColor(Result(
+            answer: '',
+            isCorrect: false,
+            hintsUsed: 0,
+            attempts: practiceScore,
+          )));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -66,7 +105,8 @@ class VocabHomePage extends StatelessWidget {
               controller: _controller,
               maxLines: 10,
               decoration: const InputDecoration(
-                hintText: 'Enter vocab pairs in the format:\n1. English - Swedish',
+                hintText:
+                    'Enter vocab pairs in the format:\n1. English - Swedish',
                 border: OutlineInputBorder(),
               ),
             ),
@@ -75,14 +115,16 @@ class VocabHomePage extends StatelessWidget {
               children: [
                 ElevatedButton(
                   onPressed: () {
-                    Provider.of<VocabProvider>(context, listen: false).addEntries(_controller.text);
+                    Provider.of<VocabProvider>(context, listen: false)
+                        .addEntries(_controller.text);
                   },
                   child: Text('Add Entries'),
                 ),
                 SizedBox(width: 10),
                 ElevatedButton(
                   onPressed: () {
-                    Provider.of<VocabProvider>(context, listen: false).resetEntries();
+                    Provider.of<VocabProvider>(context, listen: false)
+                        .resetEntries();
                   },
                   child: Text('Reset List'),
                 ),
@@ -104,25 +146,67 @@ class VocabHomePage extends StatelessWidget {
                     itemCount: vocabProvider.entries.length,
                     itemBuilder: (context, index) {
                       final entry = vocabProvider.entries[index];
+                      final latestResult = entry.results.isNotEmpty ? entry.results.last : null;
+                      final practiceScore =
+                          latestResult?.needsMorePracticeScore() ?? 0;
                       return ListTile(
-                        title: Row(
+                        title: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Expanded(child: Text('${entry.english} - ${entry.swedish}')),
-                            IconButton(
-                              icon: Icon(Icons.arrow_left),
-                              onPressed: () {
-                                Provider.of<VocabProvider>(context, listen: false).moveSeparatorLeft(index);
-                              },
-                            ),
-                            IconButton(
-                              icon: Icon(Icons.arrow_right),
-                              onPressed: () {
-                                Provider.of<VocabProvider>(context, listen: false).moveSeparatorRight(index);
-                              },
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        entry.english,
+                                        style: const TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        entry.swedish,
+                                        style: TextStyle(color: Colors.blueGrey),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                if (latestResult != null && practiceScore > 0) ...[
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text('attempts: ${latestResult.attempts}'),
+                                      Text('hints used: ${latestResult.hintsUsed}'),
+                                    ],
+                                  ),
+                                ],
+                                SizedBox(width: 10),
+                                _getPracticeIcon(practiceScore),
+                                IconButton(
+                                  icon: Icon(Icons.arrow_left),
+                                  onPressed: () {
+                                    Provider.of<VocabProvider>(context,
+                                            listen: false)
+                                        .moveSeparatorLeft(index);
+                                  },
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.arrow_right),
+                                  onPressed: () {
+                                    Provider.of<VocabProvider>(context,
+                                            listen: false)
+                                        .moveSeparatorRight(index);
+                                  },
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                        tileColor: entry.hasDash ? Colors.transparent : Colors.red.withOpacity(0.3),
+                        tileColor: entry.hasDash
+                            ? Colors.transparent
+                            : Colors.red.withOpacity(0.3),
                       );
                     },
                   ),
